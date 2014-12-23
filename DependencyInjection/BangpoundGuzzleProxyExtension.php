c3843124d82b8804916ce00e7d053702c7af5148
Benjamin Doherty <bdoherty@caxy.com>
2014-12-23 17:06:59 -0600

    create guzzle proxy client services for each endpoint.

--- DependencyInjection/BangpoundGuzzleProxyExtension.php
+++ DependencyInjection/BangpoundGuzzleProxyExtension.php
@@ -4,6 +4,7 @@ namespace Bangpound\Bundle\GuzzleProxyBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -25,6 +26,11 @@ class BangpoundGuzzleProxyExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-        $container->setParameter('bangpound_guzzle_proxy.endpoints', $config['endpoint']);
+        $baseConfig = $container->getDefinition('bangpound_guzzle_proxy.client')->getArgument(0);
+        foreach ($config['endpoints'] as $name => $clientConfig) {
+            $definition = new DefinitionDecorator('bangpound_guzzle_proxy.client');
+            $definition->replaceArgument(0, array_merge($baseConfig, $clientConfig));
+            $container->setDefinition('bangpound_guzzle_proxy.client.'.$name, $definition);
+        }
     }
 }
