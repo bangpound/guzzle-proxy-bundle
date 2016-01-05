f8e6e7ceb4f57cc121fabbad6927b483d78f8c06
Benjamin Doherty <bjd@bangpound.org>
2016-01-04 23:37:29 -0600

    code style.

--- DependencyInjection/BangpoundGuzzleProxyExtension.php
+++ DependencyInjection/BangpoundGuzzleProxyExtension.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\Loader;
 class BangpoundGuzzleProxyExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
--- Routing/ProxyLoader.php
+++ Routing/ProxyLoader.php
@@ -43,7 +43,7 @@ class ProxyLoader extends Loader
      * @param mixed  $resource A resource
      * @param string $type     The resource type
      *
-     * @return Boolean true if this class supports the given resource, false otherwise
+     * @return bool true if this class supports the given resource, false otherwise
      */
     public function supports($resource, $type = null)
     {
