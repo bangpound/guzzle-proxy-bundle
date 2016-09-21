dfafb3f6cfc5809f67a896d380861490e2f915eb
Benjamin Doherty <bjd@bangpound.org>
2016-09-20 21:53:08 -0500

    purge boilerplate.

--- Tests/Controller/ProxyControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bangpound\Bundle\GuzzleProxyBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ProxyControllerTest extends WebTestCase
-{
-}
