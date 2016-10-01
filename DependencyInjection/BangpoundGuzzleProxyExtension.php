de1a6ebf04923c0dad35c7814c22e65d5c0dd599
Benjamin Doherty <bjd@bangpound.org>
2016-10-01 14:05:31 -0500

    each proxy client is a custom configured service.

--- DependencyInjection/BangpoundGuzzleProxyExtension.php
+++ DependencyInjection/BangpoundGuzzleProxyExtension.php
@@ -4,7 +4,6 @@ namespace Bangpound\Bundle\GuzzleProxyBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -25,12 +24,5 @@ class BangpoundGuzzleProxyExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $baseConfig = $container->getDefinition('bangpound_guzzle_proxy.client')->getArgument(0);
-        foreach ($config['endpoints'] as $name => $clientConfig) {
-            $definition = new DefinitionDecorator('bangpound_guzzle_proxy.client');
-            $definition->replaceArgument(0, array_merge($baseConfig, $clientConfig));
-            $container->setDefinition('bangpound_guzzle_proxy.client.'.$name, $definition);
-        }
     }
 }
